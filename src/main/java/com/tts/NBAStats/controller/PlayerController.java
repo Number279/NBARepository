110e2f7e5a928cd1d42d25974eea80dbd9562af3
Number279 <sawyercarroll@gmail.com>
2021-03-03 15:15:53 -0600

    added swagger api tools to document api methods

--- src/main/java/com/tts/NBAStats/controller/PlayerController.java
+++ src/main/java/com/tts/NBAStats/controller/PlayerController.java
@@ -3,6 +3,10 @@ package com.tts.NBAStats.controller;
 import com.tts.NBAStats.model.Player;
 import com.tts.NBAStats.repo.PlayerRepository;
 import com.tts.NBAStats.service.PlayerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Api(value = "nbaPlayers", description = "Operations pertaining to NBA player database.")
 @RequestMapping("/player")
 public class PlayerController {
     @Autowired
@@ -42,11 +47,23 @@ public class PlayerController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved the player."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "Get Players by ID", response = Player.class,
+            responseContainer = "Individual")
     @GetMapping("/{id}")
     public Optional<Player> findById(@PathVariable Long id, Model model){
         return playerService.findById(id);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved all players."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "Get all players.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/all")
     public Iterable<Player> findAll(){
         return playerService.findAll();
@@ -62,26 +79,67 @@ public class PlayerController {
         playerService.save(player);
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved all players ordered by points."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "Sort all players by total points.", response = Player.class,
+            responseContainer = "List")
+    @GetMapping("/points")
+    public Iterable<Player> getPoints(){
+        return playerService.sortByPoints();
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved all players ordered by total rebounds."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "Sort all players by total rebounds.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/rebounds")
     public Iterable<Player> getRebounds(){
             return playerService.sortByRebounds();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved all players ordered by total assists."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "Sort all players by total assists..", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/assists")
     public Iterable<Player> getAssists(){
         return playerService.sortByAssists();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved all players ordered by total steals."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "Sort all players by total steals.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/steals")
     public Iterable<Player> getSteals(){
         return playerService.sortBySteals();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved all players ordered by total blocks."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "Sort all players by total blocks.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/blocks")
     public Iterable<Player> getBlocks(){
         return playerService.sortByBlocks();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved the position sorted by points."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "All players of a specific position sorted by total points.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/points/position")
     public List<Player> sortByPointsByPosition(@RequestParam(value="position", required = false) String position){
         if(position!=null){
@@ -89,6 +147,12 @@ public class PlayerController {
         return playerService.sortByPoints();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved the position sorted by rebounds."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "All players of a specific position sorted by total rebounds.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/rebounds/position")
     public List<Player> sortByReboundsByPosition(@RequestParam(value="position", required = false) String position){
         if(position!=null){
@@ -96,6 +160,12 @@ public class PlayerController {
         return playerService.sortByRebounds();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved the position sorted by assists."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "All players of a specific position sorted by total assists.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/assists/position")
     public List<Player> sortByAssistsByPosition(@RequestParam(value="position", required = false) String position){
         if(position!=null){
@@ -103,6 +173,12 @@ public class PlayerController {
         return playerService.sortByAssists();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved the position sorted by blocks."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "All players of a specific position sorted by total blocks.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/blocks/position")
     public List<Player> sortByBlocksByPosition(@RequestParam(value="position", required = false) String position){
         if(position!=null){
@@ -110,6 +186,12 @@ public class PlayerController {
         return playerService.sortByBlocks();
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(code=200, message="Successfully retrieved the position sorted by steals."),
+            @ApiResponse(code = 401, message = "You are not authorized to view this menu item.")
+    })
+    @ApiOperation(value = "All players of a specific position sorted by total steals.", response = Player.class,
+            responseContainer = "List")
     @GetMapping("/steals/position")
     public List<Player> sortByStealsByPosition(@RequestParam(value="position", required = false) String position){
         if(position!=null){
--- src/main/java/com/tts/NBAStats/model/Player.java
+++ src/main/java/com/tts/NBAStats/model/Player.java
@@ -1,5 +1,6 @@
 package com.tts.NBAStats.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,13 +21,30 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @ApiModelProperty(notes = "The player's first name.")
     private String firstName;
+
+    @ApiModelProperty(notes = "The player's last name.")
     private String lastName;
+
+    @ApiModelProperty(notes = "The player's position.")
     private String position;
+
+    @ApiModelProperty(notes = "The player's jersey number.")
     private int playerNumber;
+
+    @ApiModelProperty(notes = "Total points scored by the player.")
     private int points;
+
+    @ApiModelProperty(notes = "Total rebounds made by the player.")
     private Integer rebounds;
+
+    @ApiModelProperty(notes = "Total assists made by the player.")
     private int assists;
+
+    @ApiModelProperty(notes = "Total steals made by the player.")
     private int steals;
+
+    @ApiModelProperty(notes = "Total blocks made by the player.")
     private int blocks;
 }
